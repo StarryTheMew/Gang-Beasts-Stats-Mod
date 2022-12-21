827fdb47dc988d3156fdab42de9b22ca68f899bb
StarryTheMew <121196467+StarryTheMew@users.noreply.github.com>
2022-12-21 15:49:51 -0500

    Veiwing source code though this

---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Femur;
+using GB.Networking;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+namespace GangBeast_TestMod
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public GUIStyle guiStyle = new GUIStyle();
+        public GUIStyle guiStyleInvis = new GUIStyle();
+        bool Done = true;
+        float HP;
+        float Stam;
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+        }
+
+        void Update()
+        {
+        }
+
+        private bool nodie = false;
+        private void OnGUI()
+        {
+            guiStyle.fontSize = 20;
+            guiStyle.normal.textColor = Color.white;
+            guiStyleInvis.fontSize = 0;
+            guiStyleInvis.normal.textColor = Color.clear;
+            if (Done)
+            {
+                StatusHandeler[] array = (StatusHandeler[])StatusHandeler.FindObjectsOfType(typeof(StatusHandeler));
+                foreach (StatusHandeler Stats in array)
+                    if (Stats.isLocalPlayer)
+                    {
+                        HP = Stats.health;
+                        Stam = Stats.stamina;
+                    }
+                GUI.Label(new Rect(5f, 45f, 120f, 60f), "HP: " + HP, guiStyle);
+                GUI.Label(new Rect(5f, 25f, 120f, 60f), "Stamina: " + Stam, guiStyle);
+                GUI.Label(new Rect(5f, 5f, 120f, 60f), "Created By Starry", guiStyle);
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GangBeast_TestMod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.starry.gorillatag.gangbeast-testmod";
+        public const string Name = "GangBeast_TestMod";
+        public const string Version = "1.0.0";
+    }
+}
